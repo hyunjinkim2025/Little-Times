fbab9bc794c7b64c7ac7591208322027aa45350e
hyunjinkim2025 <ginitm78@gmail.com>
2025-05-11 19:36:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -46,6 +46,9 @@ uploadInput.addEventListener('change', async (event) => {
 
         const img = document.createElement('img');
         img.src = e.target.result;
+        img.style.maxHeight = "320px";
+        img.style.width = "100%";
+        img.style.objectFit = "contain";
 
         const caption = document.createElement('div');
         caption.className = 'caption';
@@ -80,10 +83,24 @@ function loadImage(file) {
 }
 
 function downloadPDF() {
-  const printArea = document.createElement("div");
-  printArea.style.padding = "1rem";
-  printArea.innerHTML = document.getElementById("album").innerHTML;
-  document.body.appendChild(printArea);
+  const albumContent = document.getElementById("album").cloneNode(true);
+  albumContent.style.display = "block";
+  albumContent.style.width = "100%";
+  albumContent.style.padding = "1rem";
+
+  albumContent.querySelectorAll("img").forEach(img => {
+    img.style.maxHeight = "320px";
+    img.style.width = "100%";
+    img.style.objectFit = "contain";
+  });
+
+  albumContent.querySelectorAll(".caption").forEach(caption => {
+    caption.style.whiteSpace = "normal";
+  });
+
+  const wrapper = document.createElement("div");
+  wrapper.appendChild(albumContent);
+  document.body.appendChild(wrapper);
 
   html2pdf().set({
     margin: 0.3,
@@ -92,11 +109,12 @@ function downloadPDF() {
     html2canvas: {
       scale: 2,
       useCORS: true,
-      scrollY: 0
+      scrollY: 0,
+      allowTaint: true
     },
     jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
-  }).from(printArea).save().then(() => {
-    document.body.removeChild(printArea);
+  }).from(wrapper).save().then(() => {
+    document.body.removeChild(wrapper);
   });
 }
 
